8d2d675a1b7cddd8e005d2ce5ea424994e0f3b76
Breno Quadros <pj_brenoquadros>
2020-03-24 20:24:10 -0300

    Criacao das entidades de Autor e Editora

---
+++ bookLibrary.Domain/Entities/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace bookLibrary.Domain.Entities
+{
+    public class Author : Entity
+    {
+        public Author(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
