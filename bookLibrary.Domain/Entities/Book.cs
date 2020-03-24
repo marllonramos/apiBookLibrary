10e74baa14b97047d92c7e22074ab6b9da3541c2
Marllon Ramos <87marllon@gmail.com>
2020-03-24 20:38:35 -0300

    Adjusting entities

--- bookLibrary.Domain/Entities/Book.cs
+++ bookLibrary.Domain/Entities/Book.cs
@@ -2,15 +2,21 @@ namespace bookLibrary.Domain.Entities
 {
     public sealed class Book : Entity
     {
-        public Book(string name, string description, int status)
+        public Book(string name, string description, int status, PublishingCompany publishingCompany, Author author, Category category)
         {
             Name = name;
             Description = description;
             Status = status;
+            PublishingCompany = publishingCompany;
+            Author = author;
+            Category = category;
         }
 
         public string Name { get; private set; }
         public string Description { get; private set; }
         public int Status { get; private set; }
+        public PublishingCompany PublishingCompany { get; private set; }
+        public Author Author { get; private set; }
+        public Category Category { get; private set; }
     }
 }
