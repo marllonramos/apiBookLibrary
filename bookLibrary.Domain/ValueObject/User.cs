ba4603e7d1b0584dcbf10987dc49e4e5ec29ca97
Marllon Ramos <87marllon@gmail.com>
2020-03-24 20:08:47 -0300

    Criacao de novas entidades e objetos de valor

---
+++ bookLibrary.Domain/ValueObject/User.cs
@@ -0,0 +1,8 @@
+namespace bookLibrary.Domain.ValueObject
+{
+    public class User
+    {
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+    }
+}
