c5ecd9c209d074c92b64f02ef550141939a76d88
Breno Quadros <pj_brenoquadros>
2020-03-25 22:42:03 -0300

    Criacao dos commands de autor e categoria e suas respectivas validacoes

---
+++ bookLibrary.Domain/Commands/AuthorCommands/UpdateAuthorCommand.cs
@@ -0,0 +1,25 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+
+namespace bookLibrary.Domain.Commands.AuthorCommands
+{
+    public class UpdateAuthorCommand : Notifiable, ICommand
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications
+            (
+                new Contract()
+                .Requires()
+                .IsNotEmpty(Id, "Id", "Informe o id do autor.")
+                .IsNullOrEmpty(Name, "Name", "Informe o nome do autor.")
+                .HasMinLen(Name, 3, "Name", "Informe um mínimo de 3 caracteres e um máximo de 30 para o autor.")
+                .HasMaxLen(Name, 30, "Name", "Informe um mínimo de 3 caracteres e um máximo de 30 para o autor.")
+            );
+        }
+    }
+}
---
+++ bookLibrary.Domain/Commands/CategoryCommands/CreateCategoryCommand.cs
@@ -0,0 +1,22 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace bookLibrary.Domain.Commands.CategoryCommands
+{
+    public class CreateCategoryCommand : Notifiable, ICommand 
+    {
+        public string Name { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications
+            (
+                new Contract()
+                .Requires()
+                .IsNotNullOrEmpty(Name, "Name", "Informe o nome da categoria.")
+                .HasMinLen(Name, 3, "Name", "Informe um mínimo de 3 caracteres e um máximo de 30 para a categoria.")
+                .HasMaxLen(Name, 30, "Name", "Informe um mínimo de 3 caracteres e um máximo de 30 para a categoria.")
+            );
+        }
+    }
+}
