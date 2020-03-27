ac9f80d3e740a66228b0ab0734b042365e30545f
Marllon Ramos <87marllon@gmail.com>
2020-03-26 21:17:58 -0300

    Implementacao dos commands e criacao do projeto de testes de unidade

--- bookLibrary.Domain/Entities/Book.cs
+++ bookLibrary.Domain/Entities/Book.cs
@@ -1,22 +1,23 @@
+using bookLibrary.Domain.Enums;
 using bookLibrary.Domain.Shared;
 
 namespace bookLibrary.Domain.Entities
 {
     public sealed class Book : Entity
     {
-        public Book(string name, string description, int status, PublishingCompany publishingCompany, Author author, Category category)
+        public Book(string title, string description, PublishingCompany publishingCompany, Author author, Category category)
         {
-            Name = name;
+            Title = title;
             Description = description;
-            Status = status;
+            Status = StatusBook.QueroComprar;
             PublishingCompany = publishingCompany;
             Author = author;
             Category = category;
         }
 
-        public string Name { get; private set; }
+        public string Title { get; private set; }
         public string Description { get; private set; }
-        public int Status { get; private set; }
+        public StatusBook Status { get; private set; }
         public PublishingCompany PublishingCompany { get; private set; }
         public Author Author { get; private set; }
         public Category Category { get; private set; }
---
+++ bookLibrary.Domain/Enums/StatusBook.cs
@@ -0,0 +1,10 @@
+﻿namespace bookLibrary.Domain.Enums
+{
+    public enum StatusBook
+    {
+        QueroComprar = 1,
+        EmEspera = 2,
+        Lendo = 3,
+        Lido = 4
+    }
+}
