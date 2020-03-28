57cbc8842d871fd6b35a64921d994d77783eeb0c
Breno Quadros <pj_brenoquadros>
2020-03-28 18:59:17 -0300

    Criacao dos testes unitarios para os commands

---
+++ bookLibrary.Domain/Commands/PublishingCompanyCommands/CreatePublishingCompanyCommand.cs
@@ -0,0 +1,21 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace bookLibrary.Domain.Commands.PublishingCompanyCommands
+{
+    public class CreatePublishingCompanyCommand : Notifiable, ICommand
+    {
+        public string Name { get; set; }
+        public void Validate()
+        {
+            AddNotifications
+            (
+                new Contract()
+                .Requires()
+                .IsNotNullOrEmpty(Name, "Name", "Informe o nome da editora.")
+                .HasMinLen(Name, 3, "Name", "Informe um mínimo de 3 caracteres e um máximo de 30 para a editora.")
+                .HasMaxLen(Name, 30, "Name", "Informe um mínimo de 3 caracteres e um máximo de 30 para a editora.")
+            );
+        }
+    }
+}
