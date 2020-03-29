6ca4bf1a6a7e00aeea475931610a2742fa59deb2
Marllon Ramos <87marllon@gmail.com>
2020-03-29 10:15:56 -0300

    Criacao dos repositorios de book e reader no dominio

---
+++ bookLibrary.Domain/Repositories/ReaderRepository.cs
@@ -0,0 +1,13 @@
+﻿using bookLibrary.Domain.Entities;
+using System;
+
+namespace bookLibrary.Domain.Repositories
+{
+    public interface ReaderRepository
+    {
+        Reader GetReader(Guid id);
+        void CreateReader(Reader reader);
+        void UpdateReader(Reader reader);
+        void DeleteReader(Guid id);
+    }
+}
