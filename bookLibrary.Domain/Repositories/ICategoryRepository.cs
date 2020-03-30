cd3fdf87871eb31626cb1655ea326c41de51da00
Breno Quadros <pj_brenoquadros>
2020-03-29 21:36:40 -0300

    Criacao dos contratos dos repositorios

---
+++ bookLibrary.Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using bookLibrary.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace bookLibrary.Domain.Repositories
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetAll();
+        Category GetById(Guid id);
+        void Create(Category category);
+        void Update(Category category);
+        void Delete(Guid id);
+    }
+}
