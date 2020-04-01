3442bb75ac5d285d8a59901d1377d5357c14cd1f
Breno Quadros <pj_brenoquadros>
2020-04-01 20:01:23 -0300

    Criacao das queries

---
+++ bookLibrary.Domain/Queries/AuthorQueries.cs
@@ -0,0 +1,14 @@
+﻿using bookLibrary.Domain.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace bookLibrary.Domain.Queries
+{
+    public static class AuthorQueries
+    {
+        public static Expression<Func<Author, bool>> GetById(Guid id)
+        {
+            return x => x.Id == id;
+        }
+    }
+}
---
+++ bookLibrary.Domain/Queries/PublishingCompanyQueries.cs
@@ -0,0 +1,14 @@
+﻿using bookLibrary.Domain.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace bookLibrary.Domain.Queries
+{
+    public static class PublishingCompanyQueries
+    {
+        public static Expression<Func<PublishingCompany, bool>> GetById(Guid id)
+        {
+            return x => x.Id == id;
+        }
+    }
+}
