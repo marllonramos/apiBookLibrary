dd50cfe5439f862cf2241a94867fc36bc88f0850
Marllon Ramos <87marllon@gmail.com>
2020-04-10 17:26:09 -0300

    Iniciando com as queries

---
+++ bookLibrary.Domain/Queries/ReaderQueries.cs
@@ -0,0 +1,16 @@
+﻿using bookLibrary.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace bookLibrary.Domain.Queries
+{
+    public static class ReaderQueries
+    {
+        //public static Expression<Func<Reader,bool>> GetBookByReader()
+        //{
+        //    return x=>x.
+        //}
+    }
+}
